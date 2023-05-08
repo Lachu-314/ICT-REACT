eaf0729d38e2746ba3f6806fbbbcb3389fa0b87e
Lachu-314 <132889608+Lachu-314@users.noreply.github.com>
2023-05-08 14:31:27 +0530

    INTENSHIP

---
+++ rough.js
@@ -0,0 +1,69 @@
+//arithmetic operation
+
+//let a=10;
+//let b = 5;
+
+    //additon
+ //   let c = a + b;
+//    console.log(c);
+
+    //multiplication
+ //   console.log(a*b);
+
+    //division
+  //  console.log(a/b);
+
+    //substraction
+  //  console.log(a-b);
+
+    //reminer
+  //  console.log(a%b);
+//--------------------------------------------------------------------------------------------
+  //string          string          string          string          string           string 
+//--------------------------------------------------------------------------------------------
+ //let fname = "amaya";
+  //let sname = "j";
+
+  //let namess = fname + sname ;
+  //let fullname = `my name is ${fname} ${sname}`;
+
+  //console.log(fullname);
+
+
+  //-------------------------------------------------------------------------------------------
+   // boolean           boolean         boolean         boolean         boolean         boolean
+  //-------------------------------------------------------------------------------------------
+
+   //let a = 1;
+   //let b = "1";
+
+   //if (a==b)
+  // {
+    //    console.log(true);
+  // }
+  // else{
+   //  console.log(false);
+   //}
+
+
+
+   //--------------------------------------------------------------------------------------
+   //OBJECTS       OBJECTS          OBJECTS         OBJECTS         OBJECTS         OBJECTS
+   //--------------------------------------------------------------------------------------
+
+   let person =
+    {
+        names : "Amaya",
+        age :  "19",
+        place: "vadakara",
+        address:
+        {
+            hname : "kv_house",
+            pname: "vaikkilasseri",
+            dname: "kozhikode",
+        }
+    };
+       // let full = `my name is ${names} and my place is ${place}`;
+        console.log(`my name is ${person.names} and my place is ${person.place}`);
+   // console.log(person.address.pname);
+
---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("AMAYA")
+//console.log("FIEDFGVB")
+var fname = "lub uu"
+{
+    console.log(fname)
+}
